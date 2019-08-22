976d004bb3a830022e47bced6d5927f19ea0fa9a
Artem123Q <36379805+Artem123Q@users.noreply.github.com>
2019-08-22 05:20:06 +0300

    Add files via upload

---
+++ homevork_2_1.py
@@ -0,0 +1,22 @@
+'''
+Task 2.1 - Exes and Ohs
+
+Check to see if a string has the same amount of 'x's and 'o's. The string can contain any char.
+
+Examples:
+
+input: "ooxx"
+output: True
+
+input: "xooxx"
+output: False
+
+input: "ooxXm"
+output: True
+
+True when no 'x' and 'o' is present
+input: "zpzpzpp"
+output: True'''
+
+some_word = input('Enter something: ')
+check_word = len(some_word)
